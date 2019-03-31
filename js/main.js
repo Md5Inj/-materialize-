784d3f50b7da951061cf127cfe56e6558321ae90
museinme <bakhir1979@gmail.com>
2019-03-31 20:20:39 +0200

    :)

--- js/main.js
+++ js/main.js
@@ -6,4 +6,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems, {});
+
+    
+  if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+      backToTop = function() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+          $('#back-to-top').removeClass('scale-out');
+        } else {
+          $('#back-to-top').addClass('scale-out');
+        }
+      };
+    backToTop();
+    $(window).on('scroll', function() {
+      backToTop();
+    });
+    
+    $('#back-to-top').on('click', function(e) {
+      e.preventDefault();
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700);
+    });
+  }
+
+  $(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      
+      var sc = $(this).attr("href"),
+          dn = $(sc).offset().top;
+
+      $('html, body').animate({scrollTop: dn}, 1000);
+    });
+  });
 });
